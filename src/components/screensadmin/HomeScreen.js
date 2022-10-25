1e1cfe82b58cf283457a200264b57960fd0679bb
tonildo123 <tony81191@gmail.com>
2022-10-25 14:47:19 -0300

    homescreen update

--- src/components/screensadmin/HomeScreen.js
+++ src/components/screensadmin/HomeScreen.js
@@ -1,12 +1,153 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  SafeAreaView,StatusBar,
+  Animated, } from 'react-native';
+  import LinearGradient from 'react-native-linear-gradient';
+
+import React from 'react';
+
+const imagenes = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0DgYxmd2FjBDhQsli2bHjRH_q5_vIVTrV3w&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdQ20ogWMgRQyXCQJoLw89XHe3ADwZvMuuBQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxAeELuDgeWUSlnp0HlMkiVXhO_-ffF8fIHQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDr2VgensIaAbMjATePK-d4umKmybEODZhkQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu4kdLh832ksa5Ai_FWiSlT5RryXnc-KVjkA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6u9JmsxAnDgGZdtS4rhDvSmTpmgm4QhnH8A&usqp=CAU",
+];
+
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+
+const ANCHO_CONTENEDOR = width * 0.7;
+const ESPACIO_CONTENEDOR = (width - ANCHO_CONTENEDOR) / 2;
+const ESPACIO = 10;
+const ALTURA_BACKDROP = height * 0.5;
+
+function Backdrop({ scrollX }) {
+  return (
+    <View
+      style={[
+        {
+          position: "absolute",
+          height: ALTURA_BACKDROP,
+          top: 0,
+          width: width,
+        },
+        StyleSheet.absoluteFillObject,
+      ]}
+    >
+      {imagenes.map((imagen, index) => {
+        const inputRange = [
+          (index - 1) * ANCHO_CONTENEDOR,
+          index * ANCHO_CONTENEDOR,
+          (index + 1) * ANCHO_CONTENEDOR,
+        ];
+
+        const opacity = scrollX.interpolate({
+          inputRange,
+          outputRange: [0, 1, 0],
+        });
+        return (
+          <Animated.Image
+            key={index}
+            source={{ uri: imagen }}
+            style={[
+              { width: width, height: ALTURA_BACKDROP, opacity },
+              StyleSheet.absoluteFillObject,
+            ]}
+          />
+        );
+      })}
+      <LinearGradient
+        colors={["transparent", "white"]}
+        style={{
+          width,
+          height: ALTURA_BACKDROP,
+          position: "absolute",
+          bottom: 0,
+        }}
+      />
+    </View>
+  );
+}
 
 const HomeScreen = () => {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
   return (
-    <View>
-      <Text>HomeScreen changeee...</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <StatusBar hidden />
+      <Backdrop scrollX={scrollX} />
+      <Animated.FlatList
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        snapToAlignment="start"
+        contentContainerStyle={{
+          paddingTop: 200,
+          paddingHorizontal: ESPACIO_CONTENEDOR,
+        }}
+        snapToInterval={ANCHO_CONTENEDOR}
+        decelerationRate={0}
+        scrollEventThrottle={16}
+        data={imagenes}
+        keyExtractor={(item) => item}
+        renderItem={({ item, index }) => {
+          const inputRange = [
+            (index - 1) * ANCHO_CONTENEDOR,
+            index * ANCHO_CONTENEDOR,
+            (index + 1) * ANCHO_CONTENEDOR,
+          ];
+
+          const scrollY = scrollX.interpolate({
+            inputRange,
+            outputRange: [0, -50, 0],
+          });
+          return (
+            <View style={{ width: ANCHO_CONTENEDOR }}>
+              <Animated.View
+                style={{
+                  marginHorizontal: ESPACIO,
+                  padding: ESPACIO,
+                  borderRadius: 34,
+                  backgroundColor: "#fff",
+                  alignItems: "center",
+                  transform: [{ translateY: scrollY }],
+                }}
+              >
+                <Image source={{ uri: item }} style={styles.posterImage} />
+                <Text style={{ fontWeight: "bold", fontSize: 26 }}>
+                  {" "}
+                  Rugby UAR 
+                </Text>
+              </Animated.View>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
+  );
 }
 
 export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  posterImage: {
+    width: "100%",
+    height: ANCHO_CONTENEDOR * 1.2,
+    resizeMode: "cover",
+    borderRadius: 24,
+    margin: 0,
+    marginBottom: 10,
+  },
+});
